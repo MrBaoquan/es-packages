0aaf49efe21143c447591363689144083fa4af78
MrBaoquan <mrma617@gmail.com>
2021-01-28 13:16:04 +0800

    add submodule mrba-cchelper.

--- packages/mrba-cchper/src/facades/Managements.ts
+++
@@ -1,9 +0,0 @@
-import { ResourceManager } from "../core/ResourceManager/ResourceManager";
-import { SceneManager } from "../core/SceneManager/SceneManager";
-import { UIManager } from "../core/UIManager/UIManager";
-
-export class Managements{
-    public static readonly UI:UIManager = UIManager.Instance;
-    public static readonly Resource:ResourceManager = new ResourceManager();
-    public static readonly Scene:SceneManager = new SceneManager();
-}
