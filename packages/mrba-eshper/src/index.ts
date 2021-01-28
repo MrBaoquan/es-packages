058dc7d784e0ba101894ea2d02bd841a7efeaaf1
MrBaoquan <mrma617@gmail.com>
2021-01-28 14:39:52 +0800

    pre add mrba-eshper.

--- packages/mrba-eshper/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./utils/ESUtils";
-export * from "./utils/Logger"
-export * from "./utils/Singleton"
-
-export function isset(target:any):boolean{
-    return target!==null&&target!==undefined;
-}
